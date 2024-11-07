568c56ca6b2183f3f42a20359bdc2ce095350490
MulawkB <167855209+MulawkB@users.noreply.github.com>
2024-11-07 17:32:04 +0100

    Add files via upload

---
+++ shop brian/shop.js
@@ -0,0 +1,91 @@
+/* - créer une tableau contenant un ensemble de produit (array ou object) avec les informations suivantes : titre, image, alt de l'image, prix, catégorie, mots clés (attributs du produit), note (utilisateur)
+    [
+        {
+            titre: 'titre produit 1',
+            image: 'image1.jpg',
+            alt: 'alt image1',
+            prix: 10,
+            categorie: 'tshirt',
+            attributs: ['attr1', 'attr2', ...],
+            note: 4   
+        },
+        {
+            titre: 'titre produit 2',
+            image: 'image2.jpg',
+            alt: 'alt image2',
+            prix: 14,
+            categorie: 'chemise',
+            attributs: ['attr1', 'attr2', ...],
+            note: 3.5   
+        },
+        ...
+    ]
+        */
+
+let produit = document.getELementById('produits');
+
+var donnees = 
+{
+    
+}
+
+
+
+/*
+    [
+        {
+            titre: 'pain',
+            image: 'img/img1.jpg',
+            alt: 'pain',
+            prix: 5,
+            categorie: 'feculent',
+            attributs: ['pain', 'bread','feculent'],
+            note: 4.7  
+        },
+        {
+            titre: 'carrotte',
+            image: 'img/img2.jpg',
+            alt: 'carrotte',
+            prix: 4,
+            categorie: 'legume',
+            attributs: ['carrotte', 'carrot','legume'],
+            note: 4.8  
+        },
+        {
+            titre: 'pasteque',
+            image: 'img/img3.jpg',
+            alt: 'pasteque',
+            prix: 8,
+            categorie: 'legume',
+            attributs: ['pasteque', 'watermelon','legume'],
+            note: 4.6  
+        },
+        {
+            titre: 'pain avec grains',
+            image: 'img/img4.jpg',
+            alt: 'pain grains',
+            prix: 6,
+            categorie: 'feculent',
+            attributs: ['pain', 'bread','feculent','grain','varient'],
+            note: 4.5  
+        },
+        {
+            titre: 'pain de campagne',
+            image: 'img/img5.jpg',
+            alt: 'pain campagne',
+            prix: 5,
+            categorie: 'feculent',
+            attributs: ['pain', 'bread','feculent','campagne','varient'],
+            note: 4.9  
+        },
+        {
+            titre: 'avocat',
+            image: 'img/img6.jpg',
+            alt: 'avocat',
+            prix: 6,
+            categorie: 'fruit',
+            attributs: ['fruit', 'avocado','exotique'],
+            note: 4.8  
+        },
+    ] 
+*/
